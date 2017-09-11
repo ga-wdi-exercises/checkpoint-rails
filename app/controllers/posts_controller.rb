a97a2107fef6a5361b5018450a76befed3ba672e
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-09-11 10:28:13 -0400

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.create!(post_params)
+		@post = Post.create!(params[:artist])
 
 		redirect_to post_path(@post)
 	end
