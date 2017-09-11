4fc86f47a6f137e286de65f524338537da55417a
Drew Dandrea <d.dandrea00@gmail.com>
2017-09-11 10:17:26 -0400

    final submission

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post_1 = Post.create(content: "I like turtles", is_published: true)
+
+post_2 = Post.create(content: "I like dogs", is_published: false)
