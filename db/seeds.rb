3549662abbd7dede4ee51d3edb5becdfbb3d2092
Spenser Holstein <spenser.holstein@gmail.com>
2017-09-11 09:44:44 -0400

    routes created, using resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+Comment.destroy_all
+
+
+post_001 = Post.create({
+  content: "Hello, I am the first post.",
+  is_published: true
+  })
+
+
+
+post_002 = Post.create({
+    content: "Good morning, I am the second post.",
+    is_published: true
+  })
