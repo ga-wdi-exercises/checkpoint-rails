6cf110a75cc9870286699430da06bc6371d3c84a
Spenser Holstein <spenser.holstein@gmail.com>
2017-09-11 10:08:44 -0400

    index and show pages working for Posts, with correct content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :posts
-resources :comments
+  root to: 'posts#index'
+
+  resources :posts
+  resources :comments
 
 end
