d5cb6972661693409c968864963839b719086fa2
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2017-09-11 10:17:38 -0400

    couldn't finish create

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.create!(post_params)
+
+		redirect_to post_path(@post)
+	end
+end
