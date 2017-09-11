5fbfc114d3ca544b6c0ef48158a66eeb97d84fce
Diona Kurien <diona.david@gmail.com>
2017-09-11 09:27:35 -0400

    completed models and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.destroy_all
+Comment.destroy_all
+post1 = Post.create({
+  content: "rails",
+  is_published: true
+
+})
+
+post2 = Post.create({
+  content: "java",
+  is_published: true
+
+})
+
+post1.comments.create({
+  body: "companies using rails"
+
+})
+
+post2.comments.create({
+  body: "Easy to use"
+  })
