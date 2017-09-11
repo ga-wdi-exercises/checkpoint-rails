fc143dc2e6d623e678ee26a90f080464799d695e
Bil Erdenekhuyag <bilgooday@gmail.com>
2017-09-11 09:30:09 -0400

    just ran seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+post_data = {
+  "post-1" => {
+    content: "This is a post.",
+    is_published: true,
+  },
+  "post-2" => {
+    content: "Another one. *Khaled Voice*",
+    is_published: true,
+  }
+}
