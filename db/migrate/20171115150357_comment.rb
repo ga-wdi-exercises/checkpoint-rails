3af923ae9b2f027154687b9854892d052d81be4d
Simplejack17 <jack.tyburski1@gmail.com>
2017-11-15 10:10:04 -0500

    base app added, no funtion

---
+++ db/migrate/20171115150357_comment.rb
@@ -0,0 +1,8 @@
+class CreateComment < ActiveRecord::Migration[5.1]
+ def change
+    create_table :comments do |t|
+     t.string :body
+     t.references :post, index: true, foreign_key: true
+     end
+   end
+end
