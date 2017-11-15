defe12ec975ac8dddbe626a022819f77b97a8f06
ANTE BOZIC <antebozic@gmail.com>
2017-11-15 09:25:29 -0500

    models&migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Comment.destroy_all
+Post.destroy_all
+
+
+Post.create(
+
+  content: "Content post 1",
+  is_published: true
+
+)
+
+Post.create(
+
+  content: "Content post 2",
+  is_published: true
+
+)
