70f073f9221e58c4f3d4a308ba7fae6f13796515
Steven Connors <steven.connors53@gmail.com>
2017-11-15 10:09:00 -0500

    Finish checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+post1 = Post.create([{content: "hopefully this works"}])
+
+post2 = Post.create([{content: "I hope I pass this checkpoint"}])
