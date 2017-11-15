a90b86886e06ff6ddf07f0f5fbc2498784397fd5
Sarah Yu <sarahyu0692@gmail.com>
2017-11-15 09:47:29 -0500

    create second post and comment for section 1 seeds requirement

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 Post.destroy_all
 Comment.destroy_all
 
-posts = Post.create(
+post_one = Post.create(
   content: "Hello world this is my first blog post.",
+  is_published: true
+)
+
+post_two = Post.create(
+  content: "Second blog post lsdkflksad",
   is_published: true
 )
 
 Comment.create(
   body: "lol",
-  post_id: posts.id
+  post_id: post_one.id
+)
+
+Comment.create(
+  body: "hahahahaha",
+  post_id: post_two.id
 )
