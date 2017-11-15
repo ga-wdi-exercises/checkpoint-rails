f473d2815378fdeceaf198403321e155c678cd77
troysipp <sipprelle_troy@bah.com>
2017-11-15 09:40:38 -0500

    finished second two

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/posts'           => 'posts#index'
+  get    '/posts/new'       => 'posts#new', as: "new_post"
+  get    '/posts/:id'       => 'posts#show', as: "post"
+  post   '/posts'           => 'posts#create'
+
 end
